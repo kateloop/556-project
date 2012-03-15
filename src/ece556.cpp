653dc7c0934330fbc7cb3ee96c469d16e89402d3
Michael Leo Starr <mstarr@tux-41.cae.wisc.edu>
2012-03-15 11:16:01 -0500

    base files

---
+++ src/ece556.cpp
@@ -0,0 +1,44 @@
+// ECE556 - Copyright 2011 University of Wisconsin-Madison.  All Rights Reserved.
+// $HeadURL:  $
+// $Id: ece556.cpp 152 2011-02-18 02:19:45Z shojaei $
+
+/*! \file ece556.cpp
+    \brief source code for the ECE556 API.
+    
+    Details.
+*/
+
+#include "ece556.h"
+
+int readBenchmark(const char *fileName, routingInst *rst){
+  /*********** TO BE FILLED BY YOU **********/  
+
+  return 1;
+}
+
+int projectTo2D(routingInst *rst){
+  /*********** TO BE FILLED BY YOU **********/  
+
+  return 1;
+}
+
+int solveRouting(routingInst *rst){
+  /*********** TO BE FILLED BY YOU **********/
+
+  return 1;
+}
+
+int writeOutput(const char *outRouteFile, routingInst *rst){
+  /*********** TO BE FILLED BY YOU **********/
+
+  return 1;
+}
+
+
+int release(routingInst *rst){
+  /*********** TO BE FILLED BY YOU **********/
+
+  return 1;
+}
+  
+
---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+// ECE556 - Copyright 2011 University of Wisconsin-Madison.  All Rights Reserved.
+// $HeadURL:  $
+// $Id: main.cpp 152 2011-02-18 02:19:45Z shojaei $
+
+
+/*! \file main.cpp
+    \brief source code for the ECE556 API.
+    
+    Details.
+*/
+
+#include "ece556.h"
+
+int main(int argc, char **argv)
+{
+
+ 	if(argc!=3){
+ 		printf("Usage : ./ROUTE.exe <input_benchmark_name> <output_file_name> \n");
+ 		return 1;
+ 	}
+
+ 	int status;
+	char *inputFileName = argv[1];
+ 	char *outputFileName = argv[2];
+
+ 	/// create a new routing instance
+ 	routingInst *rst = new routingInst;
+	
+ 	/// read benchmark
+ 	status = readBenchmark(inputFileName, rst);
+ 	if(status==0){
+ 		printf("ERROR: reading input file \n");
+ 		return 1;
+ 	}
+	
+ 	/// project into 2D
+ 	status = projectTo2D(rst);
+ 	if(status==0){
+ 		printf("ERROR: projecting into 2D \n");
+ 		release(rst);
+ 		return 1;
+ 	}
+	
+ 	/// run actual routing
+ 	status = solveRouting(rst);
+ 	if(status==0){
+ 		printf("ERROR: running routing \n");
+ 		release(rst);
+ 		return 1;
+ 	}
+	
+ 	/// write the result
+ 	status = writeOutput(outputFileName, rst);
+ 	if(status==0){
+ 		printf("ERROR: writing the result \n");
+ 		release(rst);
+ 		return 1;
+ 	}
+
+ 	release(rst);
+ 	printf("\nDONE!\n");	
+ 	return 0;
+}
