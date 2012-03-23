90db0d735f69c1d04a634c6a77d2cea60a1f2d32
Katelyn <katelyn.thode@gmail.com>
2012-03-23 15:24:29 -0500

    Now really changed net to Net

---
+++ src.ours/Net.cpp
@@ -0,0 +1,16 @@
+#include "Net.h"
+
+/*constructor*/
+Net::Net (char *name_in, int id_in, int pinNum_in, point3d *pins_in) {
+	name = name_in;
+	id = id_in;
+	pinNum = pinNum_in;
+	pins = pins_in;
+}
+	
+/*Public functions*/
+void add_route (route route) {
+	routes[routeNum] = route;
+	routeNum++;	
+}
+
